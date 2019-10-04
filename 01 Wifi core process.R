fddcdbeac8b052822571d93404dcc3e785e90306
ReneVanDijkBDH <renevdijk78@gmail.com>
2019-10-04 16:04:47 +0200

    list spaces extended

--- 01 Wifi core process.R
+++ 01 Wifi core process.R
@@ -23,7 +23,9 @@ DataBuilding2 <- ExtractData1Building(DataAllBuildings, BuildingID = 2)
 DataB0_FloorSpaces <- DataBuilding0 %>% group_by(FLOOR, SPACEID) %>% summarise(obs=n())
 
 #select small part of data
-Spacelist <- c(106,116,120)
+#Spacelist <- c(106,116,120)
+#Spacelist <- c(106,116,120,107,110,111,112)
+Spacelist <- c(106,116,120,107,110,111,112,113,114,115)
 DataB0_F0_Spaces <- ExtractData1FloorSpace(DataBuilding0, FloorID = 0,Spacelist)
 
 #rescale RSSI value
@@ -68,4 +70,14 @@ CompareResults$RegPredictLat <- RegPredictLat
 CompareResults$RegLongError <- with(CompareResults,LONGITUDE - RegPredictLong)
 CompareResults$RegLatError <- with(CompareResults,LATITUDE - RegPredictLat)
 
+# ideas:
+# Convert signal to log
+# Only include strong signals
+# Validate locations
+
+# to do:
+# plot errors
+# Validate locations
+# source function
+# Rmarkdown
 
